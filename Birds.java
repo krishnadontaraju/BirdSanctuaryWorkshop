32271ccb78871b3806c2f61a09c1d960fdfcf7f8
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-04-08 20:04:58 +0530

    Created the parent class for Birds

---
+++ Birds.java
@@ -0,0 +1,14 @@
+public abstract class Birds {
+
+    abstract public void incrementCount();
+
+    abstract int getCount();
+
+    abstract void decrementCount();
+
+    public boolean equals(Object obj) {
+
+        return super.equals(obj);
+    }
+}
+
